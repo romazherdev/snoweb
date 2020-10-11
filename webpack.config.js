359374da9ce35f1de4899c8ad8e4836b893e32d5
Raman Zherdzeu <raman_zherdzeu@epam.com>
2020-10-12 00:14:55 +0300

    Get rid of webpack-dev-server. Use `webpack serve`

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+      template: './src/index.html'
+    }),
+  ],
+});
