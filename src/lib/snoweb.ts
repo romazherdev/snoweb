bd8d6cda42da60dde46ab725f6728e5b30727951
Raman Zherdzeu <raman_zherdzeu@epam.com>
2020-10-13 00:36:50 +0300

    Render 1 snowflake

--- src/lib/snoweb.ts
+++ src/lib/snoweb.ts
@@ -1,15 +1,19 @@
 import { Destroyable, SnowebConfig } from '../models';
 import { DEFAULT_SNOWEB_CONFIG } from '../consts/default-snoweb-config';
+import { SNOWFLAKE_SVG } from '../consts/snowflake-svg';
 
 export class Snoweb implements Destroyable {
   private readonly config: SnowebConfig;
 
-  constructor(config: Partial<SnowebConfig>) {
+  constructor(config?: Partial<SnowebConfig>) {
     this.config = { ...DEFAULT_SNOWEB_CONFIG, ...config };
   }
 
   /** Begin the snowfall. Ho-ho-ho 🎅 */
   start(): void {
+    const el =document.createElement('div');
+    el.innerHTML = SNOWFLAKE_SVG;
+    document.body.appendChild(el);
     throw new Error('Method not implemented');
   }
 
