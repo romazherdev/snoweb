3942cbd099c0a87f91be1cc1f92d5f5f7062527a
Raman Zherdzeu <raman_zherdzeu@epam.com>
2020-10-15 00:36:23 +0300

    Update build config and version

--- webpack.common.js
+++ webpack.common.js
@@ -18,7 +18,11 @@ module.exports = {
     extensions: ['.ts', '.js'],
   },
   output: {
-    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'Snoweb',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
   },
 };
